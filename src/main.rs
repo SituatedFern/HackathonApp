e1b3ea0c40e10fca1187575e3b2d73d47398bf65
SituatedFern <themareepevoloutions@gmail.com>
2022-04-23 16:00:30 -0400

    added active/inactive logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use yew::prelude::*;
 
 enum Msg {
-    AddOne,
+    Inverse,
 }
 
 struct CounterComponent {
-    count: i64,
+    count: bool,
 }
 
 impl Component for CounterComponent {
@@ -13,40 +13,47 @@ impl Component for CounterComponent {
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { count: 0 }
+        Self { count: true }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.count += 1;
+            Msg::Inverse => {
+                self.count = !self.count;
                 true //re-render component
             }
         }
     }
+
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
+
+        let mut classes = vec!["hamburger"];
+        if self.count == false {
+            classes.push("is-active")
+        }
+
         html! {
             <>
                 <nav>
-                    <div class="container">
-                        <h1>{"Cmpny Name"}</h1>
+                <div class="container">
+                <h1>{"Cmpny Name"}</h1>
 
-                        <div class="menu">
-                            //add class="is-active" v
-                            <a href="#">{"Home"}</a>
-                            <a href="#">{"About"}</a>
-                            <a href="#">{"Projects"}</a>
-                            <a href="#">{"Contact"}</a>
-                        </div>
+                <div class="menu">
+                //add class="is-active" v
+                <a href="#">{"Home"}</a>
+                <a href="#">{"About"}</a>
+                <a href="#">{"Projects"}</a>
+                <a href="#">{"Contact"}</a>
+                </div>
 
-                        <button onclick={link.callback(|_| Msg::AddOne)} class="hamburger">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </button>
-                        //<p>{ self.count }</p>
-                    </div>
+                <button onclick={link.callback(|_| Msg::Inverse)} class={classes}>
+                <span></span>
+                <span></span>
+                <span></span>
+                </button>
+                //<p>{ self.count }</p>
+                </div>
                 </nav>
             </>
         }
